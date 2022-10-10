a99e339d1418eebfee6b8804381f0630ef0b5d1d
Anish <82283347+ianishk@users.noreply.github.com>
2022-10-10 06:32:12 +0530

    added inpatient and outpatient schemas

---
+++ backend/models/inpatient.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const inPatientSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: [2, "minimum length of the name is 2"],
+    maxLenght: [150, "maximum lenght of the name is 100"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: [0, "Age cannot be negative"],
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ["male", "female", "other"],
+      message: "{VALUE} is not a valid entry for Gender attribute",
+    },
+    required: true,
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  bookedRooms: {
+    type: [],
+    default: []
+  }
+});
+
+let inPatient = mongoose.model("inPatient", inPatientSchema);
+module.exports = inPatient;
