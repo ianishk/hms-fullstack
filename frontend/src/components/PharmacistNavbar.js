8d36c2b8a0451b4e22b51788d2ccd7b89bc86a8d
MM2090 <mihir2090@outlook.com>
2022-10-13 13:10:00 +0530

    update profile removed from pharmacist

--- frontend/src/components/PharmacistNavbar.js
+++ frontend/src/components/PharmacistNavbar.js
@@ -24,12 +24,12 @@ function PharmacistNavbar() {
         <li className="mt-0.5 w-full rounded border-black border-2 bg-white text-black py-3 mr-2 text-xl flex justify-center place-items-center whitespace-nowrap px-4 transition duration-300 hover:bg-black hover:text-white">
         <Link to="/pharmacist/ViewPrescription">Prescription</Link>                            
         </li>
-        <li className="mt-0.5 w-full rounded border-black border-2 bg-white text-black py-3 mr-2 text-xl flex justify-center place-items-center whitespace-nowrap px-4 transition duration-300 hover:bg-black hover:text-white" href="">
+        {/* <li className="mt-0.5 w-full rounded border-black border-2 bg-white text-black py-3 mr-2 text-xl flex justify-center place-items-center whitespace-nowrap px-4 transition duration-300 hover:bg-black hover:text-white" href="">
             <Link to="/pharmacist/EditPharmacist">Update Profile</Link>
-        </li>
+        </li> */}
        
         <li>
-        <button className="ml-[885px] bg-white rounded border-black border-2 px-3 py-3 transition duration-300 hover:bg-black hover:text-white">LOGOUT</button>
+        <button className="ml-[1090px] bg-white rounded border-black border-2 px-3 py-3 transition duration-300 hover:bg-black hover:text-white">LOGOUT</button>
         </li>
     </ul>
     
