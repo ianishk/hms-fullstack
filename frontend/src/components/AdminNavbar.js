3b37f57839079785f35e1e57fe9038d5a0720a26
rithvikshankar <rithviks@gmail.com>
2022-10-18 00:24:47 +0530

    Fixed admin navbar

--- frontend/src/components/AdminNavbar.js
+++ frontend/src/components/AdminNavbar.js
@@ -38,7 +38,9 @@ const AdminNavabar = () => {
                             <Link to="/admin/ManagePharmacist">Pharmacist</Link>
                         </li>
                         <li>
+                        <Link to="/login">
                             <button className="ml-[405px] rounded bg-white border-black border-2 px-3 py-3 transition duration-300 hover:bg-black hover:text-white">LOGOUT</button>
+                        </Link>
                         </li>
                     </ul>
 
