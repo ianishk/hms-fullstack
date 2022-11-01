538ef48f4aef0512950aa450a6cd4ca1a7f2eb21
Harshit Jadli <81504246+greywolff711@users.noreply.github.com>
2022-11-02 01:30:12 +0530

    added the admin login

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ app.use('/api/outpatient',require('./controllers/outpatientController'));
 app.use('/api/appointment',require('./controllers/appointmentController'));
 app.use('/api/bill',require('./controllers/billController'));
 app.use('/api/bookedRoom',require('./controllers/bookedRoomController'));
+app.use('/api/admin',require('./controllers/adminController'));
 
 const port = process.env.PORT || 5000;
 
