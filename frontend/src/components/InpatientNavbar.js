f2ac29e0f1966417d6f1f23886a6c63445b46875
MM2090 <mihir2090@outlook.com>
2022-11-01 11:44:01 +0530

    fixed logout button

--- frontend/src/components/InpatientNavbar.js
+++ frontend/src/components/InpatientNavbar.js
@@ -36,7 +36,7 @@ const InpatientNavabar = () => {
                         </li>
                         <li>
                         <Link to="/login">
-                            <button className="ml-[845px] rounded bg-white border-black border-2 px-3 py-3 transition duration-300 hover:bg-black hover:text-white">LOGOUT</button>
+                            <button className="rounded bg-white border-black border-2 px-3 py-3 transition duration-300 hover:bg-black hover:text-white">LOGOUT</button>
                         </Link>
                         </li>
                     </ul>
--- frontend/src/components/OutpatientNavbar.js
+++ frontend/src/components/OutpatientNavbar.js
@@ -33,7 +33,7 @@ const OutpatientNavabar = () => {
                         </li>
                         <li>
                         <Link to="/login">
-                            <button className="ml-[940px] rounded bg-white border-black border-2 px-3 py-3 transition duration-300 hover:bg-black hover:text-white">LOGOUT</button>
+                            <button className="rounded bg-white border-black border-2 px-3 py-3 transition duration-300 hover:bg-black hover:text-white">LOGOUT</button>
                         </Link>
                         </li>
                     </ul>
